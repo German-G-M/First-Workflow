5a5924782ed6a180b80bf2a1ac4bc299fc33a2a5
German-G-M <91800270+German-G-M@users.noreply.github.com>
2021-12-05 23:41:42 -0300

    Add files via upload

---
+++ workflow2p/sorteo.inc.php
@@ -0,0 +1,40 @@
+
+<div style="text-align:left; margin-left:100px; font-weight:bold; color:#023e8a">
+FLUJO:<?php echo $flujo ?>
+<br> 
+PROCESO: <?php echo $proceso ?>
+</div>
+<div style="font-weight:bold;color:#9b2226">SORTEO DE LA POSICIÓN DE LA PAPELETA</div>
+<br>
+<style>
+    table,tr,td{
+        border:solid gray 5px;
+		/*border-collapse:collapse;*/
+		text-align:center;
+		padding:30px 60px;
+		margin:20px auto;
+        color:#38b000;
+        /*background:#f5cb5c;*/
+    }
+</style>
+<table>
+    <tr>
+    <td>Posición 1</td>
+    <td>Posición 2</td>
+    <td>Posición 3</td>
+    </tr>
+    <tr>
+    <td>Posición 4</td>
+    <td>Posición 5</td>
+    <td>Posición 6</td>
+    </tr>
+</table>
+<br>
+
+
+<br/><br>
+<!--
+Nombre:
+<input type="text" name="namedocente" value=""/>
+<br/>
+-->
---
+++ workflow2p/verifica.mot.inc.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+$respuesta=$_GET["respuesta"];
+?>
